4e2a097352bb47a85ee0be805ec3d757ffc2d313
Chaterinee <chaterine.130806@gmail.com>
2025-11-10 16:45:43 +0700

    materi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         // Seed demo users for roles
         $this->call([
             UserSeeder::class,
+            KelasSeeder::class,
         ]);
     }
 }
